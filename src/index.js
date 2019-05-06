8d57a5cc24825a486a3bba00299c4f6e4a50326a
tw <t888y@qq.com>
2019-05-07 01:40:16 +0800

    getGameInfo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('123');
+const getGameInfo = () => {
+    // eslint-disable-next-line new-cap
+    const runner = window.Runner();
+    console.log(runner.currentSpeed); // 速度
+    console.log(runner.horizon.obstacles[0].spritePos); // 第一个障碍物的位置 // x: 228 y: 2
+    
+    requestAnimationFrame(getGameInfo)
+}
+
+requestAnimationFrame(getGameInfo);
