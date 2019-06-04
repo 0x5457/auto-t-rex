03fda6558722a614631fcc68bfc855b23d3f870a
tw <t888y@qq.com>
2019-06-04 23:03:10 +0800

    训练

--- src/Model.js
+++ src/Model.js
@@ -1,7 +1,8 @@
+import * as tf from '@tensorflow/tfjs';
 /**
  * 神经网络模型类
  */
-export class Model {
+export default class Model {
   /**
    * 创建模型
    */
@@ -9,7 +10,8 @@ export class Model {
     this.model = tf.sequential({
       layers: [
         tf.layers.dense({ inputShape: [3], units: 1 }),
-        tf.layers.dense({ units: 128, activation: 'relu' }),
+        tf.layers.dense({ units: 32, activation: 'relu' }),
+        tf.layers.dense({ units: 32, activation: 'relu' }),
         tf.layers.dense({ units: 2, activation: 'sigmoid' }),
       ],
     });
@@ -18,24 +20,41 @@ export class Model {
 
   /**
    * 训练模型
-   * 
+   *
    * @param {Array} data  [障碍物x轴, 障碍物宽度,  当前速度]
-   * @param {Array} label [0, 1] or [1, 0] 是否应该跳 one-hot
+   * @param {Boolean} label 是否应该跳
+   * 
+   * @return {Object} 是否应该跳的
    */
   train(data, label) {
-    this.model.fit(tf.tensor1d(data), tf.tensor1d(label), {
-      batchSize: 1,
-      epochs: 3,
-    });
+    return this.model.fit(
+      tf
+        .tensor1d(data)
+        .reshape([1, 3])
+        .softmax(),
+      tf.tensor1d(label ? [1, 0] : [0, 1]).reshape([1, 2]),
+      {
+        batchSize: 1,
+        epochs: 1,
+      },
+    );
   }
 
   /**
    * 预测是否应该跳
-   * 
+   *
    * @param {Array} data  [障碍物x轴, 障碍物宽度,  当前速度]
-   * @return {Array} 是否应该跳的 one-hot
+   * @return {Boolean} 是否应该跳的
    */
   predict(data) {
-    return this.model.predict(tf.tensor1d(data));
+    const predictRes = this.model
+      .predict(
+        tf
+          .tensor1d(data)
+          .reshape([1, 3])
+          .softmax(),
+      )
+      .arraySync();
+    return predictRes[0][0] > predictRes[0][1];
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-const getGameInfo = () => {
-    // eslint-disable-next-line new-cap
-    const runner = window.Runner();
-    // 速度
-    console.log(runner.currentSpeed);
-    // 第一个障碍物的位置 // x: 228 y: 2
-    console.log(runner.horizon.obstacles[0].spritePos);
-    
-    requestAnimationFrame(getGameInfo)
-}
+import Model from './Model';
 
-requestAnimationFrame(getGameInfo);
+const m = new Model();
+
+const getGameInfo = async () => {
+  // eslint-disable-next-line new-cap
+  const runner = window.Runner();
+
+  // 障碍物 xPos width 速度 runner.currentSpeed
+  if (runner.horizon.obstacles.length > 0) {
+    const firstHorion = runner.horizon.obstacles[0];
+    const data = [firstHorion.xPos, firstHorion.width, runner.currentSpeed];
+
+    const isJump = m.predict(data);
+
+    if (isJump) {
+      runner.tRex.startJump(runner.currentSpeed);
+    }
+
+    if (runner.crashed) {
+      console.log(runner.tRex.jumping);
+      m.train(data, !runner.tRex.jumping).then(() => {
+        runner.restart();
+      });
+    }
+  }
+};
+
+setInterval(getGameInfo, 100);
