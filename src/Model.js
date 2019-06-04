cd1d17f0047ab6429039208ce6484c955a7f2ae1
tw <t888y@qq.com>
2019-06-04 20:58:37 +0800

    完善神经网络模型

--- src/Model.js
+++ src/Model.js
@@ -1,30 +1,39 @@
 /**
- *
+ * 神经网络模型类
  */
 export class Model {
   /**
-   *
+   * 创建模型
    */
   constructor() {
     this.model = tf.sequential({
       layers: [
-        tf.layers.dense({ inputShape: [3], units: 32, activation: 'relu' }),
-        tf.layers.dense({ units: 10, activation: 'softmax' }),
+        tf.layers.dense({ inputShape: [3], units: 1 }),
+        tf.layers.dense({ units: 128, activation: 'relu' }),
+        tf.layers.dense({ units: 2, activation: 'sigmoid' }),
       ],
     });
+    this.model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy' });
   }
 
   /**
    * 训练模型
-   * @param {[]} data
-   * @param {[]} label
+   * @param {Array length 3} data  [障碍物x轴, 障碍物宽度,  当前速度]
+   * @param {Array length 2} label [0, 1] or [1, 0] 是否应该跳 one-hot
    */
   train(data, label) {
-    optimizer.minimize(() => {
-      const predYs = model(xs);
-      const loss = tf.losses.softmaxCrossEntropy(ys, predYs);
-      loss.data().then(l => console.log('Loss', l));
-      return loss;
+    this.model.fit(tf.tensor1d(data), tf.tensor1d(label), {
+      batchSize: 1,
+      epochs: 3,
     });
+  }
+
+  /**
+   * 预测是否应该跳
+   * @param {Array length 3} data  [障碍物x轴, 障碍物宽度,  当前速度]
+   * @returns 是否应该跳的 one-hot
+   */
+  predict(data) {
+    return this.model.predict(tf.tensor1d(data));
   }
 }
